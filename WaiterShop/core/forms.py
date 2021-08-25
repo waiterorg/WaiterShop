a86f5702010e63d36be12137c009c634b0094aac
waiterorg <konjkav79@gmail.com>
2021-08-25 22:36:16 +0430

    edit checkout form.

--- WaiterShop/core/forms.py
+++ WaiterShop/core/forms.py
@@ -18,22 +18,9 @@ class CheckoutForm(forms.Form):
             'class': 'custom-select d-block w-100',
         }))
     shipping_zip = forms.CharField(required=False)
-
-    billing_address = forms.CharField(required=False)
-    billing_address2 = forms.CharField(required=False)
-    billing_country = CountryField(blank_label='(select country)').formfield(
-        required=False,
-        widget=CountrySelectWidget(attrs={
-            'class': 'custom-select d-block w-100',
-        }))
-    billing_zip = forms.CharField(required=False)
-
-    same_billing_address = forms.BooleanField(required=False)
     set_default_shipping = forms.BooleanField(required=False)
     use_default_shipping = forms.BooleanField(required=False)
-    set_default_billing = forms.BooleanField(required=False)
-    use_default_billing = forms.BooleanField(required=False)
-
+    
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
 
