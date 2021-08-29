631a795a16d29c5d81a307436d0e84823b0aa755
waiterorg <konjkav79@gmail.com>
2021-08-29 19:13:19 +0430

    add satus field to item

--- WaiterShop/api/views.py
+++ WaiterShop/api/views.py
@@ -7,3 +7,8 @@ from .serializers import ItemSerializers
 class ItemViewSet(ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializers
+    filterset_fields = ['status', 'author__username','category__slug']
+    search_fields = ['title', 'author__username',
+                     'description', 'author__first_name', 'author__last_name']
+    ordering_fields = ['status', 'is_special']
+    ordering = ['-publish']
