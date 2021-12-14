340db68e0b93c736f10f3b4ca2dc8e350419a1a4
waiterorg <iwaiterorg@gmail.com>
2021-12-14 22:58:54 +0330

    remove cache from homeview

--- WaiterShop/core/urls.py
+++ WaiterShop/core/urls.py
@@ -11,7 +11,7 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 app_name = 'core'
 urlpatterns = [
-    path('', cache_page(CACHE_TTL)(HomeView.as_view()), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('product/detail/<slug:slug>', ItemDetailView.as_view(), name='item_detail'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
