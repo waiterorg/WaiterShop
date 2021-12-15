ad9b3c373022d961c6fbd0bf436114c8b5079098
waiterorg <iwaiterorg@gmail.com>
2021-12-15 13:06:02 +0330

    write product list view test

--- WaiterShop/core/tests/test_views.py
+++ WaiterShop/core/tests/test_views.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
+from core.models import Category
 from core.models import Item
 from core.models import LandingPageBanner
-
+from django.urls import reverse
 from company.models import Company
-from ..factories import ItemFactory, CompanyFactory, LandingPageFactory
+from ..factories import CategoryFactory, ItemFactory, CompanyFactory, LandingPageFactory
 
 class HomeViewTest(TestCase):
     @classmethod
@@ -35,5 +36,39 @@ class HomeViewTest(TestCase):
         self.assertIsInstance(landing_page, LandingPageBanner)
 
 class ProductListViewTest(TestCase):
-    pass
-       
+    @classmethod
+    def setUpTestData(cls):
+        number_of_item = 7
+        for i in range(number_of_item):
+            ItemFactory.create()
+
+        CategoryFactory.create()
+
+    def test_product_list_view_success(self):
+        response = self.client.get(reverse('core:product-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/product_list.html') 
+
+    def test_product_list_object_list(self):
+        response = self.client.get(reverse('core:product-list'))
+        self.assertEqual(response.status_code, 200)
+        object_list = response.context['object_list'][0]
+        self.assertIsInstance(object_list, Item)
+    
+    def test_product_list_category_context(self):
+        response = self.client.get(reverse('core:product-list'))
+        self.assertEqual(response.status_code, 200)
+        actual_category = response.context['categories'][0]
+        expect_category = Category.objects.filter(pk=1).values('title', 'slug')[0]
+        self.assertEqual(actual_category, expect_category)
+
+    def test_product_list_pagination_is_correct(self):
+        response = self.client.get(reverse('core:product-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] is True)
+        self.assertEqual(len(response.context['object_list']), 3)
+
+    def test_product_list_pagination_returns_404_page_out_of_range(self):
+        response = self.client.get(reverse('core:product-list', kwargs={'page': 999}))
+        self.assertEqual(response.status_code, 404)
