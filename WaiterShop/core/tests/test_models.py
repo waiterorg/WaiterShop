58cef1382bc92635bdc5d2f8fddaa7093a5d2eef
waiterorg <iwaiterorg@gmail.com>
2021-12-26 15:59:41 +0330

    Add comment for order item test case

--- WaiterShop/core/tests/test_models.py
+++ WaiterShop/core/tests/test_models.py
@@ -12,11 +12,11 @@ class CategoryTestCase(TestCase):
         cls.category = CategoryFactory.create(title = 'cloths i am windo64%$^')
  
     def test_str(self):
-        """category instance string"""
+        """Category instance string"""
         self.assertEqual(str(self.category), self.category.title)
 
     def test_manager_filter_active_category(self):
-        """filter active category manager for category instances with true status"""
+        """Filter active category manager for category instances with true status"""
         self.assertEqual(Category.objects.filter_active_category().count(),
                          Category.objects.filter(status=True).count())
 
@@ -29,30 +29,40 @@ class OrderItemTestCase(TestCase):
     
 
     def test_str(self):
+        """Order item instance string"""
         self.assertEqual(str(self.order_item), f"{self.order_item.quantity} of {self.order_item.item.title}")
 
     def test_order_item_name(self):
+        """Order item name method which is quantity of title"""
         actual_order_item_name = self.order_item.order_item_name()
         expected_order_item_name = f"{self.order_item.quantity} of {self.order_item.item.title}"
         self.assertEqual(actual_order_item_name, expected_order_item_name)
 
     def test_total_item_price(self):
+        """Total price for order item which is quantity multiple item price"""
         actual_total_item_price = self.order_item.get_total_item_price()
         expect_total_item_price = self.order_item.quantity * self.order_item.item.price
         self.assertEqual(actual_total_item_price, expect_total_item_price)
 
     def test_get_total_discount_item_price(self):
+        """Total discount for order item which is quantity multiple item discount price"""
         actual_total_discount_price = self.order_item.get_total_discount_item_price()
         expect_total_discount_price = self.order_item.quantity * self.order_item.item.discount_price
         self.assertEqual(actual_total_discount_price,
                          expect_total_discount_price)
 
     def test_get_amount_saved(self):
+        """Amount saved is the difference between ordinary price and discount price"""
         actual_amount_save = self.order_item.get_amount_saved()
         expected_amount_save = self.order_item.get_total_item_price() - self.order_item.get_total_discount_item_price()
         self.assertEqual(actual_amount_save, expected_amount_save)
 
     def test_get_final_price(self):
+        """
+        If item has dicount price used total discount price method
+        but if item has not disount price used total item price method
+        for calculation
+        """
         actual_final_price = self.order_item.get_final_price()
         if self.order_item.item.discount_price:
             expected_final_price = self.order_item.get_total_discount_item_price()
